5f89348b2f31879bc5159f88a7752f7c16144c28
Craig Munro <craig@eurogamer.net>
2013-12-30 13:11:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -59,9 +59,18 @@ $usage = <<<USE
 <script>
 	var image = $("demo");
 
+	// Create a new instance of ColourFinder
 	var finder = new ColourFinder();
+	
+	// Find the dominant colour in the image. Returns a hex code, i.e: "0069ff".
 	dominant = finder.getDominant(image);
+	
+	// Find the colour palette of the image. Returns an array of
+	// hex codes and instances, i.e:
+	// { "0069ff": 12, "0069aa": 14 }
 	palette = finder.getPalette(image, 8);
+	
+	// Find the average colour of the image. Returns a hex code, i.e. "00fa30".
 	average = finder.getAverage(image);	
 </script>
 USE;
